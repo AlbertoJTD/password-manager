3a0a4af696e285df1fd01abebc31a535d35168c0
AlbertoJTD <albertojtd@outlook.com>
2023-04-14 17:15:04 -0600

    Add canvas and image to the interface

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager 🔒")
+window.config(padx=20, pady=20)
 
+# Add image to the background
+canvas = Canvas(width=512, height=512)
+image = PhotoImage(file="lock.png")
+canvas.create_image(256, 150, image=image)
+canvas.pack()
 
+window.mainloop()
